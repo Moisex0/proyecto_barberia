05e8d41b0723a46a866b869113df8ac4310ae72c
Moisex0 <moises.segura.24s@utzmg.edu.mx>
2025-11-23 18:50:12 -0600

    Este archivo elimina la barberia seleccionada :)

---
+++ eliminar_barberia.php
@@ -0,0 +1,27 @@
+<?php
+include("bd.php"); // Conexión :)
+session_start();
+
+// Solo un admin puede borrar :)
+if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'admin') {
+    header("Location: login.html");
+    exit();
+}
+
+// Reviso que venga el ID :)
+if (isset($_GET['id'])) {
+    $id_barberia = intval($_GET['id']);
+
+    // Intento borrar :)
+    $ok = eliminar("DELETE FROM barberia WHERE id_barberia=$1", [$id_barberia]);
+
+    if ($ok) {
+        header("Location: barberias.php");
+        exit();
+    } else {
+        echo "Error al eliminar la barbería: " . pg_last_error();
+    }
+} else {
+    echo "No se recibió ID de barbería.";
+}
+?>
