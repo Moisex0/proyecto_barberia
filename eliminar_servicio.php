118b4cfe013fac444239fd3788f5edfa7762c8fa
Moisex0 <moises.segura.24s@utzmg.edu.mx>
2025-11-24 12:45:15 -0600

    Este elimina el servicio seleccionado :)

---
+++ eliminar_servicio.php
@@ -0,0 +1,30 @@
+<?php
+session_start(); // Inicio sesión :)
+include("bd.php"); // Conexión :)
+
+// Reviso que haya sesión :)
+if (!isset($_SESSION['usuario'])) {
+    header("Location: login.html");
+    exit();
+}
+
+// Reviso que venga el ID :)
+if (!isset($_GET['id'])) {
+    header("Location: servicios.php");
+    exit();
+}
+
+$id_servicio = intval($_GET['id']); // Lo limpio :)
+
+// Intento borrar :)
+$ok = eliminar("DELETE FROM servicio WHERE id_servicio = $1", [$id_servicio]);
+
+if (!$ok) {
+    echo "Error al eliminar el servicio: " . pg_last_error();
+    exit();
+}
+
+// Regreso al listado :)
+header("Location: servicios.php");
+exit();
+?>
